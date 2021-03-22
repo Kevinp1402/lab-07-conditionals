8806a5776f37837dd3ab2b0cb2105a2b342eaf0b
Owner <owner@kevin-mbp.myfiosgateway.com>
2021-03-22 16:45:21 -0400

    Done

---
+++ cake.py
@@ -0,0 +1,7 @@
+cakeornot = input("Do you want cake? (yes/no)")
+if cakeornot == "yes" or cakeornot == "YES":
+    print("Have some cake!")
+elif cakeornot == "no" or cakeornot == "NO":
+    print("No cake for you!")
+else:
+    print("Sorry, I dont understand.")
